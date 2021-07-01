26f1ee0a45d2e4f4aa81a8ad93481866f8186427
pratkik <86764431+pratkik@users.noreply.github.com>
2021-07-01 16:25:49 +0530

    Update separatepositivenumbers.py

--- separatepositivenumbers.py
+++ separatepositivenumbers.py
@@ -1,13 +1,15 @@
 list1= [12,-7,5,64,-14]
 for num in list1:
     if num > 0 :
-        print ([num] , end = "  ")   
-
-        
+        print ([num] , end = "  ") 
+    else :
+        pass
         
         
         
 list1= [12,14,-95,3]
 for num in list1:
     if num > 0 :
-        print ([num] , end = "  ")   
+        print ([num] , end = "  ")  
+    else :
+        pass
